e8865a4d1f9c6ec54d3c10eefc71d07feffd3b5d
oddaf <106770775+oddaf@users.noreply.github.com>
2025-06-10 00:52:08 -0300

    refactor: project structure

---
+++ cli/utils.js
@@ -0,0 +1,40 @@
+import yoctoSpinner from "yocto-spinner";
+
+/**
+ * Creates a spinner that only shows if stdout is a TTY
+ * @param {...any} args
+ * @returns {import("yocto-spinner").Spinner}
+ */
+export function ttyOnlySpinner(...args) {
+    // Only show a spinner if stdout is a TTY
+    if (process.stdout.isTTY) {
+        return yoctoSpinner(...args);
+    }
+
+    // If not a TTY, return a dummy spinner with empty chainable methods
+    return {
+        start() {
+            return this;
+        },
+        success() {
+            return this;
+        },
+        error() {
+            return this;
+        },
+    };
+}
+
+/**
+ * Converts a list of pause plans to a JSON string
+ * @param {import("./fetchPausePlans").PausePlan[]} plans
+ * @param {number} [spaces=0] Number of spaces for JSON.stringify
+ * @returns {string}
+ */
+export function createJson(plans, spaces = 0) {
+    return JSON.stringify(
+        plans,
+        (_, v) => (typeof v === "bigint" ? v.toString() : v),
+        spaces,
+    );
+}
